8d77407cf59e3fc5b123683531148abeeb36f05f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-23 20:48:08 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
+    if self.include?(".")
+      true 
+    else
+      false 
+    end 
     
   end
 
   def question?
+    if self.include?("?")
+      true 
+    else
+      false 
+    end 
 
   end
 
   def exclamation?
+    if self.include?("!")
+      true 
+    else 
+      false 
+    end 
 
   end
 
   def count_sentences
+     binding.pry 
+    self.split(".""!""?").compact.count 
 
   end
 end
